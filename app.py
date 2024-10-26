b55329912ae6dbc25eee16ac607aebf2745862e7
amparoq <aquiroz@miuandes.cl>
2024-10-26 03:07:37 -0300

    maneja edicion a los assignments

--- app.py
+++ app.py
@@ -2,19 +2,14 @@ from flask import Flask, request, jsonify, redirect, url_for, render_template
 import os
 import requests
 from database import session, LLAVE
-from models import Report, Student, Base, Assignment
-from apscheduler.schedulers.background import BackgroundScheduler
+from models import Base, Assignment
 from datetime import datetime
 import oauthlib.oauth1
-from oauthlib.oauth1 import RequestValidator, SignatureOnlyEndpoint
-from collections import defaultdict
 import schedule
 import time
 from datetime import datetime
 import threading
 
-
-# Access token para autenticar las llamadas a la API de Canvas
 ACCESS_TOKEN = "7~aAXTMm8KY9RKRQXaMPJRDAn4mrBx6tZXBHhc4akaPEUPLhUEJFverU6zWL2vwath"
 
 app = Flask(__name__)
@@ -24,11 +19,34 @@ LTI_CONSUMER_KEY = 'consumer-key'
 
 
 def check_deadline(assignment):
-    print(f"chequeando....{datetime.now()}")
-    if datetime.now() >= assignment.due_date:
-        print(f"Obteniendo submissions de assignment {assignment.id}")
-        get_submissions(assignment)
-        return schedule.CancelJob
+    print(f"Verificando el deadline para assignment {assignment.id} en curso {assignment.course_id} a las {datetime.now()}")
+    
+    # Obtener el due date actual de Canvas
+    url = f'https://canvas.instructure.com/api/v1/courses/{assignment.course_id}/assignments/{assignment.id}'
+    headers = {'Authorization': f'Bearer {ACCESS_TOKEN}'}
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        assignment_data = response.json()
+        current_due_at_str = assignment_data.get('due_at')
+        current_due_at = datetime.strptime(current_due_at_str, '%Y-%m-%dT%H:%M:%SZ')
+        
+        # Si el due date cambió, reprograma el job con el nuevo due date
+        if assignment.due_date != current_due_at:
+            print(f"El due date para assignment {assignment.id} cambió a {current_due_at}. Reprogramando tarea.")
+            assignment.due_date = current_due_at
+            session.commit()
+            schedule_check_deadline(assignment)
+            return schedule.CancelJob
+        
+        # Si el due date no ha cambiado y la fecha actual supera el due date, obten las submissions
+        if datetime.now() >= assignment.due_date:
+            print(f"Obteniendo submissions de assignment {assignment.id}")
+            get_submissions(assignment)
+            return schedule.CancelJob
+    else:
+        print(f"Error al obtener el due date actual para el assignment {assignment.id}: {response.status_code}")
+    return None
 
 # Función para obtener las entregas de un assignment
 def get_submissions(assignment):
@@ -37,14 +55,13 @@ def get_submissions(assignment):
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
-        return response.json()
+        return print(response.json())
     else:
         return {"error": f"Failed to retrieve submissions. Status code: {response.status_code}"}
 
 # Ruta principal para manejar el lanzamiento de la herramienta LTI
 @app.route('/lti_launch', methods=['POST'])
 def lti_launch():
-    # Capturar los parámetros enviados por Canvas
     course_id = request.form.get('custom_course_id')
     assignment_id = request.form.get('custom_assignment_id')
     
@@ -55,23 +72,20 @@ def lti_launch():
         if session.query(Assignment).filter_by(id=assignment_id, course_id=course_id).first().due_date is not None:
             return render_template('delivery_done.html', course_id=course_id, assignment_id=assignment_id)
 
-    # Renderizar una página HTML para mostrar en Canvas
     return render_template('delivery.html', course_id=course_id, assignment_id=assignment_id)
 
 def run_scheduler():
     while True:
         schedule.run_pending()
-        time.sleep(1)  # Verifica cada segundo si hay tareas pendientes
+        time.sleep(1)
 
 # Iniciar la tarea programada en el scheduler en el hilo principal
 def schedule_check_deadline(assignment):
-    # Programar la tarea
     schedule.every(1).minutes.do(lambda: check_deadline(assignment))
     print(f"Tarea programada para el assignment {assignment.id} en el curso {assignment.course_id}")
 
-    # Iniciar el scheduler en un hilo separado
     scheduler_thread = threading.Thread(target=run_scheduler)
-    scheduler_thread.daemon = True  # Hilo se cerrará cuando la app termine
+    scheduler_thread.daemon = True
     scheduler_thread.start()
 
 # Ruta para procesar las entregas cuando el profesor presiona el botón
@@ -95,15 +109,14 @@ def process_submissions():
         assignment = response.json()
         due_at_str = assignment.get('due_at')
         due_at = datetime.strptime(due_at_str, '%Y-%m-%dT%H:%M:%SZ')
-        print(f"due at: {due_at}")
+        print(f"Due date asignado: {due_at}")
         new_assignment.due_date = due_at
         session.commit()
         schedule_check_deadline(new_assignment)
     else:
         return "Hubo un error", 401
-
     
-    return "Las entregas se enviaran cuando la tarea termine su deadline", 200
+    return "Las entregas se enviarán cuando la tarea llegue a su deadline actual", 200
 
 # Función para crear las tablas de la base de datos
 def crear_tablas():
